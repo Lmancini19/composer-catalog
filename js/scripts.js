26e384ba58e9270eeba30ef958ab886ca014fb73
Lmancini19 <laura.m.webdev@gmail.com>
2023-03-24 21:20:35 -0400

    Add first object array for Pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+//this is the first JS file for my pokedex app 
+let pokemonList = [ 
+    { name: 'Bulbasaur', height: "2'4", type: ['Grass', 'Poison'] },
+    { name: 'Squirtle', height: "1'8", type: ['Water'] },
+    { name: 'Charmander', height: "2'", type: ['Fire']}
+];
