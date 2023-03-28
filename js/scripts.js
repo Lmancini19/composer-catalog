ea2dbd7bd5b9880d84a10e777b6fd844d20703ae
Lmancini19 <laura.m.webdev@gmail.com>
2023-03-27 20:10:07 -0400

    add for loop and if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-//this is the first JS file for my pokedex app 
+// list of pokemon
 let pokemonList = [ 
-    { name: 'Bulbasaur', height: "2'4", type: ['Grass', 'Poison'] },
-    { name: 'Squirtle', height: "1'8", type: ['Water'] },
-    { name: 'Charmander', height: "2'", type: ['Fire']}
+    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] },
+    { name: 'Charmander', height: 0.6, type: ['Fire']},
+    { name: 'Squirtle', height: 0.5, type: ['Water'] }    
 ];
+
+// loop over pokemonList and write each pokemon to html, if pokemon is over 0.7m add additional message 
+for (let i =  0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >= 0.7) {
+        document.write("<p>" + pokemonList[i].name + " (height: " +  pokemonList[i].height + ")" + " -Wow, that's big! </p>")
+    } else {
+        document.write("<p>" + pokemonList[i].name + " (height: " +  pokemonList[i].height + ")</p>")
+    }
+}
+
